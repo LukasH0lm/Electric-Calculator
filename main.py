d48d61fe590345a73d5b314b9b316a3d2f20a504
LukasHolm420 <lukasholm@hotmail.com>
2020-10-28 18:48:05 +0100

    Added looping

--- main.py
+++ main.py
@@ -23,18 +23,21 @@ def find_w():
         print("\nusing the formula P = U^2 / Ohm")
         print("\nW = ")
         print(V ** 2 / R)
+        end()
     if known == 2:
         R = float(input("Enter R: "))
         A = float(input("Enter A: "))
         print("\nusing the formula P = Ohm * I^2")
         print("\nW = ")
         print(R * A ** 2)
+        end()
     if known == 3:
         A = float(input("Enter A: "))
         V = float(input("Enter V: "))
         print("\nusing the formula P = U * I")
         print("\nW = ")
         print(V * A)
+        end()
 
 
 def find_v():
@@ -49,18 +52,21 @@ def find_v():
         print("\nusing the formula U = Ohm * I")
         print("\nV = ")
         print(R * A)
+        end()
     if known == 2:
         A = float(input("Enter A: "))
         W = float(input("Enter W: "))
         print("\nusing the formula U = P / I")
         print("\nV = ")
         print(W / A)
+        end()
     if known == 3:
         W = float(input("Enter W: "))
         R = float(input("Enter R: "))
         print("\nusing the formula U = sqrt(P * Ohm)")
         print("\nV = ")
         print(math.sqrt(W * R))
+        end()
 
 
 def find_a():
@@ -75,18 +81,21 @@ def find_a():
         print("\nusing the formula I = sqrt(P / Ohm) ")
         print("\nA = ")
         print(math.sqrt(W/R))
+        end()
     if known == 2:
         W = float(input("Enter W: "))
         V = float(input("Enter V: "))
         print("\nusing the formula I = P / U")
         print("\nA = ")
         print(W / V)
+        end()
     if known == 3:
         V = float(input("Enter V: "))
         R = float(input("Enter R: "))
         print("\nusing the formula I = U / Ohm")
         print("\nA = ")
         print(V / R)
+        end()
 
 
 def find_r():
@@ -101,18 +110,27 @@ def find_r():
         print("\nusing the formula Ohm = U / I")
         print("\nR = ")
         print(V / A)
+        end()
     if known == 2:
         A = float(input("Enter A: "))
         W = float(input("Enter W: "))
         print("\nusing the formula Ohm = U^2 / P")
         print("\nR = ")
         print(W ** 2 / A)
+        end()
     if known == 3:
         W = float(input("Enter W: "))
         A = float(input("Enter A: "))
         print("\nusing the formula Ohm = P / I^2")
         print("\nR = ")
         print(W / A ** 2)
+        end()
+
+def end():
+    print("\ncontinue...")
+    input()
+    print("\n\n\n")
+    main()
 
 
 if __name__ == "__main__":
